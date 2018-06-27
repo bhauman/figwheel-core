988e573f8029b3ea8a6daf0ffa6b140eb25edfab
Bruce Hauman <bhauman@gmail.com>
2018-06-27 14:43:21 -0400

    release 0.1.4
    this is to establish a new home for the repository

--- project.clj
+++ project.clj
@@ -1,9 +1,8 @@
-(defproject com.bhauman/figwheel-core "0.1.4-SNAPSHOT"
+(defproject com.bhauman/figwheel-core "0.1.4"
   :description "Figwheel core provides code reloading facilities for ClojureScript."
   :url "https://github.com/bhauman/figwheel-core"
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-
   :scm {:name "git"
         :url "https://github.com/bhauman/figwheel-core"}
 
