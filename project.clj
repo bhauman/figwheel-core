10ff8477dbf02da83712bdd84d94f885276bdb77
Bruce Hauman <bhauman@gmail.com>
2018-12-22 10:06:28 -0500

    bump to 0.2.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.bhauman/figwheel-core "0.2.0"
+(defproject com.bhauman/figwheel-core "0.2.1-SNAPSHOT"
   :description "Figwheel core provides code reloading facilities for ClojureScript."
   :url "https://github.com/bhauman/figwheel-core"
   :license {:name "Eclipse Public License - v 1.0"
